a75a5e2321b6d1cf7efb522053234b653e45c50a
Awais <razhar5214@bths.edu>
2022-01-20 14:05:40 -0500

    issue #1 route to Home Page with react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Link, Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/about" element={<h1>About Page!</h1>}/>
+        </Routes>
     </div>
   );
 }
 
-export default App;
+
