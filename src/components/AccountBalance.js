7e9d9d3365b9208ce6dd29c8b707a9d42e76a3b8
Farai Major <faraimutukumira@gmail.com>
2022-01-24 01:03:04 -0500

    assignment done

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -4,7 +4,7 @@ export default function AccountBalance(props) {
 
     return (
         <div>
-            <h2> Account Balance: $ {props.accountBalance.toFixed(2)}</h2>
+            <h2> Account Balance: $ {props.accountBalance}</h2>
         </div>
     )
 }
