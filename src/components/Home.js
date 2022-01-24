62c72efeab2dd5068e0c92ee87f2dbeac408ccd8
Farai Major <faraimutukumira@gmail.com>
2022-01-24 00:55:11 -0500

    closes #1, closes #2, closes #3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,3 @@
-// import React from "react"
-// import AccountBalance from "./AccountBalance"
-// import { Link } from "react-router-dom"
-
-
-// export default function Home(props) {
-
-//     return (
-//         <div>
-//             <h1>Bank of React</h1>
-//             <Link to="/userProfile">User Profile</Link>
-//             <br /><br />
-//             <Link to="/debits">Debits</Link>
-//             <br /><br />
-//             <Link to="/credits">Credits</Link>
-//             <br /><br />
-//             <Link to="/login">Login</Link>
-//             <hr />
-//             <AccountBalance accountBalance={props.accountBalance} />
-//         </div>
-//     )
-// }
 import React from 'react';
 import { Link } from 'react-router-dom';
 
